b5e010397d20fef849325ecde6947fdf8192924c
suljaxm <ujxhz@outlook.com>
2019-08-13 21:57:36 +0800

    feat: dispaly system
    print system's OS, Kernel, CPU, Memory, Total Processes, Running Processes and Up Time

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,13 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long total_seconds) {
+    int seconds_in_day_remainder = total_seconds % 86400;
+    int hours = int(seconds_in_day_remainder / 86400.0 * 24);
+    int minutes = int((((seconds_in_day_remainder / 86400.0) * 24) - hours) * 60);
+    int seconds = int((((((seconds_in_day_remainder / 86400.0) * 24) - hours) * 60) - minutes) * 60);
+
+    char buffer[9];
+    sprintf(buffer, "%02d:%02d:%02d", hours, minutes, seconds);
+    return buffer;
+}
