c81066ab46bc464f4521cdf8a6d323fe0a307268
suljaxm <ujxhz@outlook.com>
2019-08-26 18:22:20 +0800

    add getNumberOfCores() into ProcessParser.h

--- Process.h
+++ Process.h
@@ -46,5 +46,15 @@ string Process::getProcess(){
     this->upTime = ProcessParser::getProcUpTime(this->pid);
     this->cpu = ProcessParser::getCpuPercent(this->pid);
 
-    return (this->pid + "   " + //TODO: finish the string! this->user + "   "+ mem...cpu...upTime...;
+    return (this->pid + "   "
+                    + this->user
+                    + "   "
+                    + this->mem.substr(0,5)
+                    + "     "
+                    + this->cpu.substr(0,5)
+                    + "     "
+                    + this->upTime.substr(0,5)
+                    + "    "
+                    + this->cmd.substr(0,30)
+                    + "...");
 }
